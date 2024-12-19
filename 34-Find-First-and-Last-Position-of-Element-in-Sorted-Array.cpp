4846c98a610ad4509906094dd79e5e3f460a255b
Adarsh Roy <156921139+adarshroy001@users.noreply.github.com>
2024-12-19 11:47:35 +0530

    [34] [Time Beats: 100.00%] [Memory Beats: 19.51%] - LeetPush

--- 34-Find-First-and-Last-Position-of-Element-in-Sorted-Array.cpp
+++ 34-Find-First-and-Last-Position-of-Element-in-Sorted-Array.cpp
@@ -3,21 +3,31 @@ public:
     vector<int> searchRange(vector<int>& nums, int target) {
         int n = nums.size() ; 
         int low = 0 , high = n-1 , start = n , end = -1 ;
-        // while(high>= low){
-        //     int mid = low + (high-low)/2 ; 
-        //     if(arr[mid] == target){
-        //         start = mid ; 
-        //     }
-        //     if(arr[mid] < target){
-        //         low = mid + 1 ;
-        //     }
-        // }
-        for(int i = 0 ; i<n ; i++){
-            if(nums[i] == target){
-               if(start > i) start = i ;
-               if(end < i) end = i ;
+        //finding first occurence 
+        while(high>= low){
+            int mid = low + (high-low)/2 ; 
+            if(nums[mid] < target){
+                low = mid + 1 ;
+            }
+            else {
+                if(start > mid && nums[mid]==target) {start = mid ;}
+                high = mid -1 ;
             }
         }
+        //finding last occurence 
+        low = 0 ;
+        high = n-1;
+        while(high>= low){
+            int mid = low + (high-low)/2 ; 
+            if(nums[mid] <= target){
+               low = mid + 1 ;
+              if(end < mid  && nums[mid]==target ){ end = mid ;}
+            }
+            else {
+                high = mid -1 ;
+            }
+        }
+     
          if(start == n) start = -1;
          return {start,end};
         
