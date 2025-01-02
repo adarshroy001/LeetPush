c5d8782d0f619351d56c1ba9cf97d6c5d6068a69
Adarsh Roy <156921139+adarshroy001@users.noreply.github.com>
2025-01-02 21:51:53 +0530

    [33] [Time Beats: 100.00%] [Memory Beats: 40.82%] - LeetPush

--- 33-Search-in-Rotated-Sorted-Array.cpp
+++ 33-Search-in-Rotated-Sorted-Array.cpp
@@ -3,12 +3,29 @@ public:
     int search(vector<int>& nums, int target) {
         int n = nums.size()  ;
         int index = - 1 ; 
-        for(int i = 0 ; i < n ; i++){
-            if(nums[i] == target){
-                index = i ; 
+        int low = 0 , high =  n - 1 ;
+        while(high>=low){
+            int mid = low + (high-low)/2 ;
+            if(nums[mid]==target) return mid ;
+            if( nums[low] <= nums[mid]){
+                  if(nums[low] <= target && target < nums[mid] ){
+                    high = mid- 1 ;
+                  }
+                  else{
+                  low = mid + 1 ;
+                  }
+            }
+            else{
+                  if(nums[high]>=target && target > nums[mid] ){
+                    low = mid + 1; 
+                  }
+                  else{
+                    high = mid - 1; 
+                  }
             }
         }
+
         
-        return index ;
+        return -1 ;
     }
 };
