9fbd7223a2f5c1af322ee01f6a8053d7ede32687
Adarsh Roy <156921139+adarshroy001@users.noreply.github.com>
2025-08-14 03:49:53 +0530

    [141] [Time Beats: 6.26%] [Memory Beats: 9.06%] - LeetPush

--- Linked-List-Cycle.cpp
+++ Linked-List-Cycle.cpp
@@ -9,17 +9,30 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        bool flag = false ; 
-        ListNode* slow = head , *fast = head ; 
-        if(head == NULL ) return false ; 
-        if(head->next == NULL) return false ; 
-        if( head->next->next == NULL) return false ; 
-        while(fast != NULL && fast->next != NULL  ){
-            slow = slow->next ; 
-            fast = fast->next->next ; 
-            if(slow == fast ) return true ; 
+
+        // most optimal
+
+        // bool flag = false ; 
+        // ListNode* slow = head , *fast = head ; 
+        // if(head == NULL ) return false ; 
+        // if(head->next == NULL) return false ; 
+        // if( head->next->next == NULL) return false ; 
+        // while(fast != NULL && fast->next != NULL  ){
+        //     slow = slow->next ; 
+        //     fast = fast->next->next ; 
+        //     if(slow == fast ) return true ; 
             
+        // }
+        // return flag ; 
+
+        //Brute force 
+        map<ListNode*,int> m ; 
+        ListNode* temp = head ; 
+        while(temp != NULL){
+          if(m.find(temp) != m.end()) return true ; 
+          m[temp] ++ ; 
+          temp = temp->next ; 
         }
-        return flag ; 
+        return false ; 
     }
 };
