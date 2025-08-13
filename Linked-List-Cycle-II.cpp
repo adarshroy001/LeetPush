cb3bba03553080c888c25b43c099801575f41c2e
Adarsh Roy <156921139+adarshroy001@users.noreply.github.com>
2025-08-14 05:05:31 +0530

    [142] [Time Beats: 73.73%] [Memory Beats: 53.51%] - LeetPush

--- Linked-List-Cycle-II.cpp
+++ Linked-List-Cycle-II.cpp
@@ -23,7 +23,6 @@ public:
         // Optimal (Reducing space to o(1)) slow and Fast 
         ListNode* slow = head ; 
         ListNode* fast = head ; 
-        if(head ==  NULL || head->next == NULL) return NULL   ;
         while(fast != NULL && fast->next != NULL){
             slow = slow->next  ; 
             fast = fast->next->next ; 
