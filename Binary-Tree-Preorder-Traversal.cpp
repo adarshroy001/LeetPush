575f69359a04d442aa9faa9a0a55acafbfe9cc29
Adarsh Roy <156921139+adarshroy001@users.noreply.github.com>
2025-10-12 16:38:12 +0530

    [144] [Time Beats: 0.68%] [Memory Beats: 5.02%] - LeetPush

--- Binary-Tree-Preorder-Traversal.cpp
+++ Binary-Tree-Preorder-Traversal.cpp
@@ -11,18 +11,35 @@
  */
 class Solution {
 public:
+    vector<int> preorderTrav(TreeNode* root , vector<int> &v){
+       if(root == NULL) return v ; 
+       v.push_back(root->val) ; 
+       preorderTrav(root->left,v) ; 
+       preorderTrav(root->right,v) ;
+       return v ; 
+    }
+
     vector<int> preorderTraversal(TreeNode* root) {
-        stack<TreeNode *> st ; 
+        //Itrative (we need a stack Tc->O(N))
+    //     stack<TreeNode *> st ; 
+    //     vector<int> v ;
+    //     if(root == NULL) return v ; 
+    //     st.push(root) ; 
+    //     while(!st.empty()){
+    //       TreeNode* temp = st.top() ; 
+    //       st.pop() ; 
+    //       v.push_back(temp->val) ; 
+    //       if(temp->right != NULL ) st.push(temp->right) ; 
+    //       if(temp->left != NULL ) st.push(temp->left) ;
+    //     }
+    //     return v ;
+    // 
+    
+    
+     //Recussive 
         vector<int> v ;
-        if(root == NULL) return v ; 
-        st.push(root) ; 
-        while(!st.empty()){
-          TreeNode* temp = st.top() ; 
-          st.pop() ; 
-          v.push_back(temp->val) ; 
-          if(temp->right != NULL ) st.push(temp->right) ; 
-          if(temp->left != NULL ) st.push(temp->left) ;
-        }
-        return v ;
+        preorderTrav(root , v) ; 
+        return v; 
+    
     }
 };
