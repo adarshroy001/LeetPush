a7291a8d58f2e6b59b749ee1e2b7e04b3b1c47cf
Adarsh Roy <156921139+adarshroy001@users.noreply.github.com>
2025-10-23 20:59:47 +0530

    [39] [Time Beats: 54.57%] [Memory Beats: 37.54%] - LeetPush

--- Combination-Sum.cpp
+++ Combination-Sum.cpp
@@ -3,21 +3,17 @@ public:
     vector<vector<int>> ans ; 
 
     void helper(vector<int>& candi , vector<int>& v , int tar , int i){
-        if(tar == 0){
-            ans.push_back(v) ;
-            return ;
-        }
         if(i == candi.size()){
             if(tar == 0){
                 ans.push_back(v) ; 
             }
             return ;
         }
-        if(tar < 0) return ;
-        // Logic
-        v.push_back(candi[i]) ;
-        helper(candi,v,tar-candi[i],i) ;
-        v.pop_back() ;
+        if(tar >= 0){
+            v.push_back(candi[i]) ;
+            helper(candi,v,tar-candi[i],i) ;
+            v.pop_back() ;
+        }
         helper(candi,v,tar,i+1) ;
 
     } 
