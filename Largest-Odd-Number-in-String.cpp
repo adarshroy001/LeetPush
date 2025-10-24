5c73fd7cd9d678f0864dbf6ef3c9e447353622f4
Adarsh Roy <156921139+adarshroy001@users.noreply.github.com>
2025-10-24 14:38:08 +0530

    [1903] [Time Beats: 100.00%] [Memory Beats: 99.74%] - LeetPush

---
+++ Largest-Odd-Number-in-String.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+       for(int i = num.size()-1 ; i >= 0 ; i--){
+        if(num[i] == '1' || num[i] == '3' || num[i] == '5' || num[i] == '7' || num[i] == '9'){
+            return num ; 
+        }else{
+            num.pop_back();
+        }
+       } 
+       return "";
+    }
+};
