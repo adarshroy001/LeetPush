ea0dffdc868bd04fae12dbe0fa74dd5a5ccd0935
Adarsh Roy <156921139+adarshroy001@users.noreply.github.com>
2025-10-27 19:44:51 +0530

    [199] [Time Beats: 100.00%] [Memory Beats: 7.89%] - LeetPush

--- Binary-Tree-Right-Side-View.cpp
+++ Binary-Tree-Right-Side-View.cpp
@@ -9,29 +9,52 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+ //M-1 using level order traversal and putting rightmost element in ans vector 
+// class Solution {
+// public:
+//     vector<int> rightSideView(TreeNode* root) {
+//         if(!root) return {}; 
+//         queue<TreeNode*> q ; 
+//         q.push(root) ; 
+//         vector<vector<int>> v ;
+//         while(!q.empty()){
+//             int size = q.size() ; 
+//             vector<int> ans ; 
+//             for(int i = 0 ; i < size ; ++i){
+//                  TreeNode* front = q.front() ; 
+//                  q.pop() ; 
+//                  ans.push_back(front->val) ; 
+//                  if(front->left) q.push(front->left) ; 
+//                  if(front->right) q.push(front->right) ; 
+//             }
+//             v.push_back(ans) ;
+//         }
+//         vector<int> res(v.size()) ; 
+//         for(int i = 0 ; i < v.size() ; i++){
+//             res[i] = v[i][v[i].size()-1] ;
+//         }
+//         return res ;
+//     }
+// };
+
+
+//M-2 using recurssion and map and checking corrosponding to key
 class Solution {
 public:
+    map<int , int >  m ; 
+    vector<int> ans ; 
+    void helper(TreeNode* root , int lvl){
+        if(!root) return ; 
+        if(!m.count(lvl)){
+            m[lvl] ++  ;
+            ans.push_back(root->val) ; 
+        }
+        helper(root->right, lvl+1) ;
+        helper(root->left , lvl+1) ;
+    }
     vector<int> rightSideView(TreeNode* root) {
-        if(!root) return {}; 
-        queue<TreeNode*> q ; 
-        q.push(root) ; 
-        vector<vector<int>> v ;
-        while(!q.empty()){
-            int size = q.size() ; 
-            vector<int> ans ; 
-            for(int i = 0 ; i < size ; ++i){
-                 TreeNode* front = q.front() ; 
-                 q.pop() ; 
-                 ans.push_back(front->val) ; 
-                 if(front->left) q.push(front->left) ; 
-                 if(front->right) q.push(front->right) ; 
-            }
-            v.push_back(ans) ;
-        }
-        vector<int> res(v.size()) ; 
-        for(int i = 0 ; i < v.size() ; i++){
-            res[i] = v[i][v[i].size()-1] ;
-        }
-        return res ;
+        helper(root,0) ; 
+        return ans ; 
     }
 };
